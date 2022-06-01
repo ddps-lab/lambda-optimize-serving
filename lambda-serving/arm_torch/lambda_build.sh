c500c95b12ecc1a6e7f75407b702cbf8dee3fc52
최재강 <chl8273@naver.com>
2022-06-01 17:15:09 +0900

    add delete scenario to automate

--- lambda-serving/arm_torch/lambda_build.sh
+++ lambda-serving/arm_torch/lambda_build.sh
@@ -11,6 +11,12 @@ docker tag $IMAGE_NAME $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
 
+aws ecr delete-repository \
+    --repository-name $IMAGE_NAME \
+    --force
+
+sleep 5
+
 aws ecr create-repository \
     --repository-name $IMAGE_NAME \
     --image-scanning-configuration scanOnPush=true \
@@ -18,8 +24,15 @@ aws ecr create-repository \
 
 docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME
 
-aws lambda create-function --region us-west-2 --function-name ${IMAGE_NAME} \
+aws lambda delete-function \
+    --function-name $IMAGE_NAME
+
+sleep 5
+
+aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME \
             --package-type Image  \
             --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME:latest   \
             --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
-            --architectures arm64
+            --architectures arm64 \
+            --memory-size 2048 \
+            --timeout 120
