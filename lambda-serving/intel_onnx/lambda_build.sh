a48fc539c43824ee9a89ca49096625ea53dae7d7
최재강 <chl8273@naver.com>
2022-06-01 18:00:40 +0900

    change img name hw type

--- lambda-serving/intel_onnx/lambda_build.sh
+++ lambda-serving/intel_onnx/lambda_build.sh
@@ -1,4 +1,4 @@
-export IMAGE_NAME="lambda_optimize_serving"
+export IMAGE_NAME="serving_intel_onnx"
 
 sudo chmod 666 /var/run/docker.sock
 sudo service docker start
@@ -11,4 +11,28 @@ docker tag $IMAGE_NAME $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
 
+aws ecr delete-repository \
+    --repository-name $IMAGE_NAME \
+    --force
+
+sleep 5
+
+aws ecr create-repository \
+    --repository-name $IMAGE_NAME \
+    --image-scanning-configuration scanOnPush=true \
+    --region us-west-2
+
 docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME
+
+aws lambda delete-function \
+    --function-name $IMAGE_NAME
+
+sleep 5
+
+aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME \
+            --package-type Image  \
+            --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME:latest   \
+            --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+            --architectures x86_64 \
+            --memory-size 2048 \
+            --timeout 120
