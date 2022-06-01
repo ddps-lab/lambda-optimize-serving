2dc79322ec7e88227a3aa9eceb16c54930488f5f
최재강 <chl8273@naver.com>
2022-06-01 17:45:01 +0900

    change tvm

--- lambda-serving/arm_tvm/lambda_function.py
+++ lambda-serving/arm_tvm/lambda_function.py
@@ -1,5 +1,3 @@
-# image-classification converter 
-
 import time
 import json
 from json import load
@@ -10,96 +8,17 @@ import boto3
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
 
-def timer(thunk, repeat=1, number=10, dryrun=3, min_repeat_ms=1000):
-    """Helper function to time a function"""
-    for i in range(dryrun):
-        thunk()
-    ret = []
-    for _ in range(repeat):
-        while True:
-            beg = time.time()
-            for _ in range(number):
-                thunk()
-            end = time.time()
-            lat = (end - beg) * 1e3
-            if lat >= min_repeat_ms:
-                break
-            number = int(max(min_repeat_ms / (lat / number) + 1, number * 1.618))
-        ret.append(lat / number)
-    return ret
-
-def load_model(mtype, model_name, batchsize):
+def load_model(model_name, batchsize):
     s3_client = boto3.client('s3')
 
-    if mtype == "base":
-        import torch
-        os.makedirs(os.path.dirname(f'/tmp/base/{model_name}/'), exist_ok=True)
-        s3_client.download_file(BUCKET_NAME, f'torch/base/{model_name}/model.pt', f'/tmp/base/{model_name}/model.pt')
-        s3_client.download_file(BUCKET_NAME, f'torch/base/{model_name}/model_state_dict.pt',
-                                f'/tmp/base/{model_name}/model_state_dict.pt')
+    os.makedirs(os.path.dirname(f'/tmp/tvm/{model_name}/'), exist_ok=True)
+    s3_client.download_file(BUCKET_NAME, f'models/tvm/arm/{model_name}/{model_name}_{batchsize}.tar',
+                            f'/tmp/tvm/{model_name}/{model_name}_{batchsize}.tar')
 
-        PATH = f"/tmp/base/{model_name}/"
-        model = torch.load(PATH + 'model.pt')
-        model.load_state_dict(torch.load(PATH + 'model_state_dict.pt'))
-
-    elif mtype == "onnx":
-        os.makedirs(os.path.dirname(f'/tmp/onnx/{model_name}/'), exist_ok=True)
-        s3_client.download_file(BUCKET_NAME, f'torch/onnx/{model_name}/{model_name}_{batchsize}.onnx',
-                                f'/tmp/onnx/{model_name}/{model_name}_{batchsize}.onnx')
-
-        # onnx 는 model path 필요 
-        model = f"/tmp/onnx/{model_name}/{model_name}_{batchsize}.onnx"
-
-    elif mtype == "tvm":
-        os.makedirs(os.path.dirname(f'/tmp/tvm/{model_name}/'), exist_ok=True)
-        s3_client.download_file(BUCKET_NAME, f'torch/tvm/arm/{model_name}/{model_name}_{batchsize}.tar',
-                                f'/tmp/tvm/{model_name}/{model_name}_{batchsize}.tar')
-
-        model = f"/tmp/tvm/{model_name}/{model_name}_{batchsize}.tar"
+    model = f"/tmp/tvm/{model_name}/{model_name}_{batchsize}.tar"
 
     return model
 
-
-def base_serving(model_name, batchsize, imgsize=224, repeat=10):
-    import torch
-    # random data
-    input_shape = (batchsize, 3, imgsize, imgsize)
-    data_array = np.random.uniform(0, 255, size=input_shape).astype("float32")
-    torch_data = torch.tensor(data_array)
-
-    model = load_model("base", BUCKET_NAME, model_name, batchsize)
-    model.eval()
-    
-    res = timer(lambda: model(torch_data),
-                repeat=repeat,
-                dryrun=5,
-                min_repeat_ms=1000)
-    return res
-
-
-def onnx_serving(model_name, batchsize, imgsize=224, repeat=10):
-    import onnxruntime as ort
-
-    model_path = load_model("onnx", BUCKET_NAME, model_name, batchsize)
-
-    session = ort.InferenceSession(model_path)
-    session.get_modelmeta()
-    inname = [input.name for input in session.get_inputs()]
-    outname = [output.name for output in session.get_outputs()]
-
-    if model_name == "inception_v3":
-        imgsize = 299
-    image_shape = (3, imgsize, imgsize)
-    data_shape = (batchsize,) + image_shape
-    data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
-    for i in range(repeat):
-        start_time = time.time()
-        session.run(outname, {inname[0]: data})
-        running_time = time.time() - start_time
-        time_list.append(running_time)
-        
-    res = np.median(np.array(time_list[1:]))
-    return res
 
 def tvm_serving(model_name, batchsize, imgsize=224, repeat=10):
     import tvm
@@ -112,7 +31,7 @@ def tvm_serving(model_name, batchsize, imgsize=224, repeat=10):
     input_shape = (batchsize, 3, imgsize, imgsize)
     output_shape = (batchsize, 1000)
 
-    model_path = load_model("tvm", BUCKET_NAME, model_name, batchsize)
+    model_path = load_model(model_name, batchsize)
     loaded_lib = tvm.runtime.load_module(model_path)
 
     dev = tvm.cpu()
@@ -126,39 +45,31 @@ def tvm_serving(model_name, batchsize, imgsize=224, repeat=10):
 
 def lambda_handler(event, context):
     start_time = time.time()
-    # event = event['body-json']
-
     model_name = event['model_name']
-    compiler_type = event['compiler_type']
+    hardware = event['hardware']
+    framework = event['framework']
+    optimizer = event['optimizer']
+    lambda_memory = event['lambda_memory']
     batchsize = event['batchsize']
+    user_email = event['user_email']
+    convert_time = event['convert_time']
 
-    
-    if compiler_type == "onnx":
-        print("Torch model to ONNX model serving")
-        res = onnx_serving(model_name, batchsize)
-
-    elif compiler_type == "tvm":
+    if optimizer == "tvm" and hardware == "arm":
         res = tvm_serving(model_name, batchsize)
-
-    elif compiler_type == "base":
-        res = base_serving(model_name, batchsize)
-    running_time = time.time() - start_time
-    return {'handler_time': running_time,
-           'average_inference_time': res}
-
-# test 
-# bucket_name = ''
-# model_name = 'mobilenet_v2'
-# compiler_type='base'
-# batchsize = 1
-
-
-# if compiler_type == "onnx":
-#     print("Torch model to ONNX model serving")
-#     onnx_serving(bucket_name,model_name,batchsize)
-
-# elif compiler_type == "tvm":
-#     tvm_serving(bucket_name,model_name,batchsize)
-
-# elif compiler_type == "base":
-#     base_serving(bucket_name,model_name,batchsize)
+        running_time = time.time() - start_time
+        return {
+            'model_name': model_name,
+            'hardware': hardware,
+            'framework': framework,
+            'optimizer': optimizer,
+            'lambda_memory': lambda_memory,
+            'batchsize': batchsize,
+            'user_email': user_email,
+            'execute': True,
+            'convert_time': convert_time,
+            'inference_time': running_time
+        }
+    else:
+        return {
+            'execute': False
+        }
