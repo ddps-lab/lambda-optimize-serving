917b37679880e48010ad13f287da6bfffc06c551
Subin <39542978+subeans@users.noreply.github.com>
2022-07-14 16:20:24 +0900

    delete ses_function

--- lambda-optimize/intel_tvm/lambda_function.py
+++ lambda-optimize/intel_tvm/lambda_function.py
@@ -63,35 +63,6 @@ def optimize_tvm(model,model_name,batchsize,model_size,imgsize=224,layout="NHWC"
     print("S3 upload done")
 
     return convert_time
-    
-def ses_send(user_email,info):
-    dst_format = {"ToAddresses":[f"{user_email}"],
-    "CcAddresses":[],
-    "BccAddresses":[]}
-
-    dfile_path = "/tmp/destination.json"
-
-    with open(dfile_path, 'w', encoding='utf-8') as file:
-        json.dump(dst_format, file)
-
-    message_format = {
-                        "Subject": {
-                            "Data": "AYCI : AllYouCanInference results mail",
-                            "Charset": "UTF-8"
-                        },
-                        "Body": {
-                            "Text": {
-                                "Data": f"AYCI convert time results\n---------------------------------------\n{info['model_name']} convert using TVM on Intel Done!\n{info['model_name']} size : {info['model_size']} MB\nConvert {info['model_name']} latency : {round(info['convert_time'],4)} s",
-                                "Charset": "UTF-8"
-                            },
-                        }
-                    }
-    mfile_path = "/tmp/message.json"
-
-    with open(mfile_path, 'w', encoding='utf-8') as mfile:
-        json.dump(message_format, mfile)
-
-    os.system("aws ses send-email --from allyoucaninference@gmail.com --destination=file:///tmp/destination.json --message=file:///tmp/message.json")
 
 def lambda_handler(event, context):    
     model_name = event['model_name']
@@ -103,17 +74,6 @@ def lambda_handler(event, context):
     user_email = event ['user_email']
     lambda_memory = event['lambda_memory']
     convert_time = 0
-    info = {
-                'model_name': model_name,
-                'model_size': model_size,
-                'hardware': hardware,
-                'framework': framework,
-                'optimizer': optimizer,
-                'lambda_memory': lambda_memory,
-                'batchsize': batchsize,
-                'user_email': user_email,
-                'convert_time': convert_time
-            }
 
     if "intel" in hardware and "tvm" in optimizer:
         start_time = time.time()
@@ -123,9 +83,6 @@ def lambda_handler(event, context):
 
         print("Hardware optimize - Torch model to TVM model")
         convert_time = optimize_tvm(model,model_name,batchsize,model_size)
-        info['convert_time'] = convert_time
-
-        ses_send(user_email,info)
 
     return {
             'model_name': model_name,
