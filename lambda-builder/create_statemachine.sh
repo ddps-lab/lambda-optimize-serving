d4412448953c3f3f7191b6373cf56ab949eb82f9
manchann <chl8273@naver.com>
2022-07-15 16:08:41 +0900

    region 변경

--- lambda-builder/create_statemachine.sh
+++ lambda-builder/create_statemachine.sh
@@ -2,7 +2,7 @@ lambda_memory="512 1024 2048 4096 8192 10240"
 
 for lm in $lambda_memory; do
   aws stepfunctions create-state-machine --name "ayci_"$lm \
-      --region us-wset-2 \
+      --region us-west-2 \
       --role-arn arn:aws:iam::741926482963:role/service-role/StepFunctions-Serverless_Inference_System-role-a3b1f8ca \
       --definition "$(cat "statemachine-"$lm".json")"
 
