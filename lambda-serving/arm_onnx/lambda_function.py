d421df09043a0b126b235da17600e5712512dae7
manchann <chl8273@naver.com>
2022-07-28 14:10:56 +0900

    nlp workload 추가 완료

--- lambda-serving/arm_onnx/lambda_function.py
+++ lambda-serving/arm_onnx/lambda_function.py
@@ -4,6 +4,7 @@ from json import load
 import numpy as np
 import os
 import boto3
+import onnxruntime as ort
 
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
@@ -21,9 +22,7 @@ def load_model(model_name, model_size):
     return model
 
 
-def onnx_serving(model_name, model_size, batchsize, imgsize=224, repeat=10):
-    import onnxruntime as ort
-
+def onnx_serving(wtype, model_name, model_size, batchsize, imgsize=224, repeat=10):
     model_path = load_model(model_name, model_size)
 
     session = ort.InferenceSession(model_path)
@@ -31,23 +30,38 @@ def onnx_serving(model_name, model_size, batchsize, imgsize=224, repeat=10):
     inname = [input.name for input in session.get_inputs()]
     outname = [output.name for output in session.get_outputs()]
 
-    if model_name == "inception_v3":
-        imgsize = 299
-    image_shape = (3, imgsize, imgsize)
-    data_shape = (batchsize,) + image_shape
-    data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+    input_data = {}
+
+    if wtype == 'img':
+        if model_name == "inception_v3":
+            imgsize = 299
+        image_shape = (3, imgsize, imgsize)
+        data_shape = (batchsize,) + image_shape
+        data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+        input_data = {inname[0]: data}
+
+    elif wtype == 'nlp':
+        dtype = "float32"
+        seq_length = 128
+        inputs = np.random.randint(0, 2000, size=(batchsize, seq_length)).astype(dtype)
+        token_types = np.random.uniform(size=(batchsize, seq_length)).astype(dtype)
+        valid_length = np.asarray([seq_length] * batchsize).astype(dtype)
+
+        input_data = {inname[0]: inputs, inname[1]: token_types, inname[2]: valid_length}
 
     time_list = []
     for i in range(repeat):
         start_time = time.time()
-        session.run(outname, {inname[0]: data})
+        session.run(outname, input_data)
         running_time = time.time() - start_time
         time_list.append(running_time)
 
     res = np.median(np.array(time_list[1:]))
     return res
 
+
 def lambda_handler(event, context):
+    workload_type = event['workload_type']
     model_name = event['model_name']
     model_size = event['model_size']
     hardware = "arm"
@@ -60,13 +74,13 @@ def lambda_handler(event, context):
     request_id = context.aws_request_id
     log_group_name = context.log_group_name
 
-   
     if "onnx" in optimizer:
         start_time = time.time()
-        res = onnx_serving(model_name, model_size, batchsize)
+        res = onnx_serving(workload_type, model_name, model_size, batchsize)
         running_time = time.time() - start_time
 
         return {
+            'workload_type': workload_type,
             'model_name': model_name,
             'model_size': model_size,
             'hardware': "arm",
@@ -78,8 +92,8 @@ def lambda_handler(event, context):
             'execute': True,
             'convert_time': convert_time,
             'inference_time': running_time,
-            'request_id' : request_id,
-            'log_group_name' : log_group_name
+            'request_id': request_id,
+            'log_group_name': log_group_name
         }
     else:
         return {
--- lambda-serving/arm_tvm/lambda_function.py
+++ lambda-serving/arm_tvm/lambda_function.py
@@ -4,14 +4,17 @@ from json import load
 import numpy as np
 import os
 import boto3
+import tvm
+from tvm import relay
+import tvm.contrib.graph_executor as runtime
 
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
 
-def load_model(framework,model_name, model_size):
+def load_model(framework, model_name, model_size):
     load_start = time.time()
     s3_client = boto3.client('s3')
-    
+
     os.makedirs(os.path.dirname(f'/tmp/tvm/'), exist_ok=True)
     if "onnx" in framework:
         s3_client.download_file(BUCKET_NAME, f'models/tvm/arm/onnx/{model_name}_{model_size}.tar',
@@ -25,28 +28,38 @@ def load_model(framework,model_name, model_size):
     return model
 
 
-def tvm_serving(framework,model_name, model_size, batchsize, imgsize=224, repeat=10):
-    import tvm
-    from tvm import relay
-    import tvm.contrib.graph_executor as runtime
-
-    input_name = "input0"
-    if model_name == "inception_v3":
-        imgsize == 299
-    input_shape = (batchsize, 3, imgsize, imgsize)
-    output_shape = (batchsize, 1000)
-
-    model_path = load_model(framework,model_name, model_size)
-    loaded_lib = tvm.runtime.load_module(model_path)
-    
-    #target = "llvm -device=arm_cpu -mtriple=aarch64-linux-gnu"
-    #dev = tvm.device(target, 0)
+def tvm_serving(wtype, framework, model_name, model_size, batchsize, imgsize=224, repeat=10):
     dev = tvm.cpu()
+    model_path = load_model(framework, model_name, model_size)
+    loaded_lib = tvm.runtime.load_module(model_path)
     module = runtime.GraphModule(loaded_lib["default"](dev))
-    data = np.random.uniform(-1, 1, size=input_shape).astype("float32")
-    data = tvm.nd.array(data, dev)
-    module.set_input(input_name, data)
-    
+
+    if wtype == 'img':
+        if model_name == "inception_v3":
+            imgsize = 299
+        input_shape = (batchsize, 3, imgsize, imgsize)
+        output_shape = (batchsize, 1000)
+        input_name = "input0"
+
+        # target = "llvm -device=arm_cpu -mtriple=aarch64-linux-gnu"
+        # dev = tvm.device(target, 0)
+        data = np.random.uniform(-1, 1, size=input_shape).astype("float32")
+        data = tvm.nd.array(data, dev)
+        module.set_input(input_name, data)
+
+    elif wtype == 'nlp':
+        dtype = "float32"
+        seq_length = 128
+        inputs = np.random.randint(0, 2000, size=(batchsize, seq_length)).astype(dtype)
+        token_types = np.random.uniform(size=(batchsize, seq_length)).astype(dtype)
+        dtype = 'float32'
+        valid_length = np.asarray([seq_length] * batchsize).astype(dtype)
+
+        data = tvm.nd.array(inputs, dev)
+        token_types_nd = tvm.nd.array(token_types, dev)
+        valid_length_nd = tvm.nd.array(valid_length, dev)
+        module.set_input(data0=data, data1=token_types_nd, data2=valid_length_nd)
+
     time_list = []
     for i in range(repeat):
         start_time = time.time()
@@ -57,8 +70,9 @@ def tvm_serving(framework,model_name, model_size, batchsize, imgsize=224, repeat
     res = np.median(np.array(time_list[1:]))
     return res
 
+
 def lambda_handler(event, context):
-    start_time = time.time()
+    workload_type = event['workload_type']
     model_name = event['model_name']
     model_size = event['model_size']
     hardware = "arm"
@@ -73,10 +87,11 @@ def lambda_handler(event, context):
 
     if "tvm" in optimizer:
         start_time = time.time()
-        res = tvm_serving(framework,model_name, model_size, batchsize)
+        res = tvm_serving(workload_type, framework, model_name, model_size, batchsize)
         running_time = time.time() - start_time
 
         return {
+            'workload_type': workload_type,
             'model_name': model_name,
             'model_size': model_size,
             'hardware': "arm",
@@ -88,8 +103,8 @@ def lambda_handler(event, context):
             'execute': True,
             'convert_time': convert_time,
             'inference_time': running_time,
-            'request_id' : request_id,
-            'log_group_name' : log_group_name
+            'request_id': request_id,
+            'log_group_name': log_group_name
         }
     else:
         return {
