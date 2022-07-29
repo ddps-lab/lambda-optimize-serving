cab525bc2c22cc24ea3559e820e49e522eedf671
manchann <chl8273@naver.com>
2022-07-29 14:53:03 +0900

    add optimizer 자동화 스크립트

--- lambda-builder/build_test_lambdas.sh
+++ lambda-builder/build_test_lambdas.sh
@@ -7,44 +7,65 @@ arm_serving="arm_onnx arm_torch arm_tvm"
 intel_serving="intel_onnx intel_torch intel_tvm"
 
 for serv in $arm_serving; do
-    aws lambda delete-function \
+  aws lambda delete-function \
     --function-name $IMAGE_NAME'_'$serv
 
-    sleep 5
+  sleep 5
 
-    aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
-      --package-type Image \
-      --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv:latest \
-      --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
-      --architectures arm64 \
-      --memory-size 2048 \
-      --timeout 240
+  aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --package-type Image \
+    --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv:latest \
+    --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+    --architectures arm64 \
+    --memory-size 2048 \
+    --timeout 240
 
-    sleep 60
+  sleep 60
 
-    aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
-      --environment "Variables={BUCKET_NAME=ayci}"
+  aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --environment "Variables={BUCKET_NAME=ayci}"
 done
 
 for serv in $intel_serving; do
-    aws lambda delete-function \
+  aws lambda delete-function \
     --function-name $IMAGE_NAME'_'$serv
 
-    sleep 5
+  sleep 5
 
-    aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
-      --package-type Image \
-      --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv:latest \
-      --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
-      --architectures x86_64 \
-      --memory-size 2048 \
-      --timeout 240
+  aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --package-type Image \
+    --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv:latest \
+    --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+    --architectures x86_64 \
+    --memory-size 2048 \
+    --timeout 240
 
-    sleep 60
+  sleep 60
 
-    aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
-      --environment "Variables={BUCKET_NAME=ayci}"
-  done
+  aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --environment "Variables={BUCKET_NAME=ayci}"
 done
 
+export IMAGE_NAME="convert_torch"
 
+converting="intel_tvm arm_tvm intel_onnx"
+
+for serv in $converting; do
+  aws lambda delete-function \
+    --function-name $IMAGE_NAME'_'$serv
+
+  sleep 5
+
+  aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --package-type Image \
+    --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv:latest \
+    --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+    --architectures x86_64 \
+    --memory-size 4096 \
+    --timeout 240
+
+  sleep 60
+
+  aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --environment "Variables={BUCKET_NAME=ayci}"
+done
