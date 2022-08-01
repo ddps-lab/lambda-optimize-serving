ec8d1c8bd7be50e6a80139ad35be0899630ba178
manchann <chl8273@naver.com>
2022-08-01 14:51:02 +0900

    arm lambda 환경 변경

--- lambda-builder/build_test_lambdas.sh
+++ lambda-builder/build_test_lambdas.sh
@@ -48,46 +48,54 @@ done
 
 export IMAGE_NAME="convert_torch"
 
-converting="intel_tvm arm_tvm"
+aws lambda delete-function \
+  --function-name $IMAGE_NAME'_intel_onnx'
+aws lambda delete-function \
+  --function-name $IMAGE_NAME'_arm_onnx'
+aws lambda delete-function \
+  --function-name $IMAGE_NAME'_intel_tvm'
+aws lambda delete-function \
+  --function-name $IMAGE_NAME'_arm_tvm'
+
+sleep 5
+
+aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_intel_onnx' \
+  --package-type Image \
+  --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_intel_onnx':latest \
+  --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+  --architectures x86_64 \
+  --memory-size 4096 \
+  --timeout 240
+
+aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_arm_onnx' \
+  --package-type Image \
+  --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_arm_onnx':latest \
+  --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+  --architectures arm64 \
+  --memory-size 4096 \
+  --timeout 240
+
+aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_intel_tvm' \
+  --package-type Image \
+  --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_intel_tvm':latest \
+  --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+  --architectures x86_64 \
+  --memory-size 4096 \
+  --timeout 240
+
+aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_arm_tvm' \
+  --package-type Image \
+  --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_arm_tvm':latest \
+  --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+  --architectures arm64 \
+  --memory-size 4096 \
+  --timeout 240
+
+sleep 60
+
+converting="intel_onnx arm_onnx intel_tvm arm_tvm"
 
 for serv in $converting; do
-  aws lambda delete-function \
-    --function-name $IMAGE_NAME'_'$serv
-
-  sleep 5
-
-  aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
-    --package-type Image \
-    --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv:latest \
-    --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
-    --architectures x86_64 \
-    --memory-size 4096 \
-    --timeout 240
-
-  sleep 60
-
-  aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
-    --environment "Variables={BUCKET_NAME=ayci}"
-done
-
-onnx="intel_onnx arm_onnx"
-
-for serv in $onnx; do
-  aws lambda delete-function \
-    --function-name $IMAGE_NAME'_'$serv
-
-  sleep 5
-
-  aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
-    --package-type Image \
-    --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_onnx':latest \
-    --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
-    --architectures x86_64 \
-    --memory-size 4096 \
-    --timeout 240
-
-  sleep 60
-
   aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
     --environment "Variables={BUCKET_NAME=ayci}"
 done
