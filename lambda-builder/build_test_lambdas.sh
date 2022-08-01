6ff8920f717425b520abe9f4687c2d73e42365be
manchann <chl8273@naver.com>
2022-08-01 14:18:33 +0900

    onnx의 경우 따로 처리

--- lambda-builder/build_test_lambdas.sh
+++ lambda-builder/build_test_lambdas.sh
@@ -48,7 +48,7 @@ done
 
 export IMAGE_NAME="convert_torch"
 
-converting="intel_tvm arm_tvm intel_onnx"
+converting="intel_tvm arm_tvm"
 
 for serv in $converting; do
   aws lambda delete-function \
@@ -59,6 +59,28 @@ for serv in $converting; do
   aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
     --package-type Image \
     --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv:latest \
+    --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
+    --architectures x86_64 \
+    --memory-size 4096 \
+    --timeout 240
+
+  sleep 60
+
+  aws lambda update-function-configuration --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --environment "Variables={BUCKET_NAME=ayci}"
+done
+
+onnx="intel_onnx arm_onnx"
+
+for serv in $onnx; do
+  aws lambda delete-function \
+    --function-name $IMAGE_NAME'_'$serv
+
+  sleep 5
+
+  aws lambda create-function --region us-west-2 --function-name $IMAGE_NAME'_'$serv \
+    --package-type Image \
+    --code ImageUri=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_onnx':latest \
     --role arn:aws:iam::$ACCOUNT_ID:role/jg-efs-role \
     --architectures x86_64 \
     --memory-size 4096 \
