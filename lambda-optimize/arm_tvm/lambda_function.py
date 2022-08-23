f9c49712f5774b1b12d0c0b4d7522b0712213c60
Subin <39542978+subeans@users.noreply.github.com>
2022-08-23 15:19:06 +0900

    update convert time to s3

--- lambda-optimize/arm_tvm/lambda_function.py
+++ lambda-optimize/arm_tvm/lambda_function.py
@@ -24,6 +24,21 @@ def check_results(prefix,model_size,model_name,batchsize):
             break
     return exist 
 
+def update_results(framework,model_name,model_size,batchsize,convert_time,load_time):
+    info = {'base_model_load_time':load_time,
+            'convert_time' : convert_time}
+
+    with open(f'/tmp/{model_name}_{model_size}_{batchsize}_convert.json','w') as f:
+        json.dump(info, f, ensure_ascii=False, indent=4)  
+    
+    if "onnx" in framework : 
+        s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_convert.json',BUCKET_NAME,f'results/tvm/arm/onnx/{model_name}_{model_size}_{batchsize}_convert.json')
+        print("upload done : convert time results")
+    else:
+        s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_convert.json',BUCKET_NAME,f'results/tvm/arm/{model_name}_{model_size}_{batchsize}_convert.json')
+        print("upload done : convert time results")
+
+
 def load_model(framework,model_name,model_size):    
     import onnx
 
@@ -42,10 +57,16 @@ def load_model(framework,model_name,model_size):
    
     return model
 
-def optimize_tvm(wtype,framework, model,model_name,batchsize,model_size,imgsize=224,seq_length=128, layout="NCHW"):
+def optimize_tvm(wtype,framework,model_name,batchsize,model_size,imgsize=224,seq_length=128, layout="NCHW"):
     import tvm
     from tvm import relay
 
+    ######0. model load 
+    start_time = time.time()
+    model = load_model(framework,model_name,model_size)
+    load_time = time.time() - start_time
+
+    ######1. make dataset 
     # ImageClf input 
     if wtype == "img":
         if model_name == "inception_v3":
@@ -62,6 +83,7 @@ def optimize_tvm(wtype,framework, model,model_name,batchsize,model_size,imgsize=
         tokens_tensor = torch.tensor(np.array([inputs]))
         segments_tensors = torch.tensor(np.array([token_types]))
 
+    ######2. make model to tvm format 
     if "onnx" in framework:   
         framework="onnx"
         if wtype == "img":
@@ -96,7 +118,8 @@ def optimize_tvm(wtype,framework, model,model_name,batchsize,model_size,imgsize=
     else:
         assert layout == "NCHW"
 
-    target = "llvm -mcpu=core-avx2"
+    #######3. tvm optimize 
+    target = tvm.target.arm_cpu()
     
     convert_start_time = time.time()
     with tvm.transform.PassContext(opt_level=3,required_pass=["FastMath"]):
@@ -104,28 +127,18 @@ def optimize_tvm(wtype,framework, model,model_name,batchsize,model_size,imgsize=
         lib = relay.build(mod, target=target, params=params)
     convert_time = time.time() - convert_start_time
 
+    ######4. tvm export and upload to s3
+    os.makedirs(os.path.dirname(f'/tmp/tvm/arm/{model_name}/'), exist_ok=True)    
+    lib.export_library(f"/tmp/tvm/arm/{model_name}/{model_name}_{batchsize}.tar")
+    print("export done :",f"{model_name}_{batchsize}.tar")
 
     if framework=="onnx":
-        prefix = f'models/tvm/intel/onnx/'
-        exist = check_results(prefix,model_size,model_name,batchsize)
-        if exist==False:
-            os.makedirs(os.path.dirname(f'/tmp/tvm/intel/{model_name}/'), exist_ok=True)
-            lib.export_library(f"/tmp/tvm/intel/{model_name}/{model_name}_{batchsize}.tar")
-            print("export done :",f"{model_name}_{batchsize}.tar")
-            s3_client.upload_file(f'/tmp/tvm/intel/{model_name}/{model_name}_{batchsize}.tar',BUCKET_NAME,f'models/tvm/intel/onnx/{model_name}_{model_size}_{batchsize}.tar')
-            print("S3 upload done")
-
+        s3_client.upload_file(f'/tmp/tvm/arm/{model_name}/{model_name}_{batchsize}.tar',BUCKET_NAME,f'models/tvm/arm/onnx/{model_name}_{model_size}_{batchsize}.tar')
     else:
-        prefix = f'models/tvm/intel/'
-        exist = check_results(prefix,model_size,model_name,batchsize)
-        if exist==False:
-            os.makedirs(os.path.dirname(f'/tmp/tvm/intel/{model_name}/'), exist_ok=True)
-            lib.export_library(f"/tmp/tvm/intel/{model_name}/{model_name}_{batchsize}.tar")
-            print("export done :",f"{model_name}_{batchsize}.tar")
-            s3_client.upload_file(f'/tmp/tvm/intel/{model_name}/{model_name}_{batchsize}.tar',BUCKET_NAME,f'models/tvm/intel/{model_name}_{model_size}_{batchsize}.tar')
-            print("S3 upload done")
+        s3_client.upload_file(f'/tmp/tvm/arm/{model_name}/{model_name}_{batchsize}.tar',BUCKET_NAME,f'models/tvm/arm/{model_name}_{model_size}_{batchsize}.tar')
+    print("S3 upload done")
 
-    return convert_time
+    return load_time, convert_time
 
 def lambda_handler(event, context):    
     workload_type = event['workload_type']
@@ -136,25 +149,27 @@ def lambda_handler(event, context):
     batchsize = event['batchsize']
     user_email = event ['user_email']
     lambda_memory = event['lambda_memory']
-    convert_time = 0
 
-    if "tvm" in configuration["intel"] :
-        start_time = time.time()
-        model = load_model(framework,model_name,model_size)
-        load_time = time.time() - start_time
-        print("Model load time : ",load_time)
+    ##### 1.  convert한 모델이 있는지 확인 
+    if "onnx" in framework:
+        prefix = 'models/tvm/arm/onnx/'
+    else:
+        prefix = 'models/tvm/arm/'
+    exist = check_results(prefix,model_size,model_name,batchsize)
 
-        print(f"Hardware optimize - {framework} model to TVM model")
-        convert_time = optimize_tvm(workload_type,framework,model,model_name,batchsize,model_size)
+    ##### 2. 없다면 convert 
+    if exist == False:
+        if "tvm" in configuration["arm"] :
+            load_time, convert_time = optimize_tvm(workload_type,framework,model_name,batchsize,model_size)
+            update_results(framework, model_name,model_size,batchsize,convert_time,load_time)
 
     return {
             'workload_type':workload_type,
             'model_name': model_name,
             'model_size': model_size,
-            'configuration': event['configuration'],
             'framework': framework,
-            'lambda_memory': lambda_memory,
+            'configuration': event['configuration'],
             'batchsize': batchsize,
             'user_email': user_email,
-            'convert_time': convert_time
+            'lambda_memory': lambda_memory
         }
